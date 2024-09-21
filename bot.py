de0f2a1fd9a4cf942fa489ac96353347942ef52e
Mayur Jadhav <63432459+mayurjadhav2002@users.noreply.github.com>
2024-09-21 14:33:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,15 @@
 import os
 import asyncio
+import logging
 from telethon import TelegramClient, events, functions, types
 from dotenv import load_dotenv
-import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import re
 
 load_dotenv()
 
+logging.basicConfig(level=logging.INFO)
 
 bot_token = os.getenv('TELEGRAM_BOT_API_KEY')
 api_id = os.getenv('TELEGRAM_APP_API_ID')
@@ -16,50 +18,47 @@ api_hash = os.getenv('TELEGRAM_APP_API_HASH')
 session_dir = 'store'
 clients = []
 
-
-
-
 async def check_command(event, client):
+    """Check for the /group command and handle group creation."""
     sender = await event.get_sender()
     me = await client.get_me()
 
     if sender.id != me.id:
         return
 
-    if (event.message.text.startswith("/group") and 
-        event.message.to_id.user_id != sender.id and 
-        event.message.from_id.user_id == sender.id):
-        
+    if event.message.text.startswith("/group") and event.message.to_id.user_id != sender.id:
         try:
-            if event.message:
-                kaithia_message = await event.message.edit(f"<b>@kaithia_bot</b> is creating the <code>/group</code>...", parse_mode='HTML')
+            kaithia_message = await event.message.edit(f"<b>@kaithia_bot</b> is creating the <code>/group</code>...", parse_mode='HTML')
 
-                command_parts = event.message.text.split(" ", 1)
-                group_command = command_parts[1] if len(command_parts) > 1 else ""
+            command_parts = event.message.text.split(" ", 1)
+            group_command = command_parts[1] if len(command_parts) > 1 else ""
 
-                add_kaithia = "@kaithia" in group_command
-                group_command = group_command.replace("@kaithia", "").strip() 
-                
-                recipient_id = event.message.to_id.user_id
-                recipient = await event.client.get_entity(recipient_id)
-                recipient_first_name = getattr(recipient, 'first_name', None)
+            add_kaithia = "@kaithia" in group_command
+            group_command = group_command.replace("@kaithia", "").strip() 
+            
+            recipient_id = event.message.to_id.user_id
+            recipient = await event.client.get_entity(recipient_id)
+            recipient_first_name = getattr(recipient, 'first_name', None)
 
-                group_name = group_command if group_command else f"{sender.first_name} <> {recipient_first_name}"
-                participants = [sender, recipient]
+            group_name = group_command if group_command else f"{sender.first_name} <> {recipient_first_name}"
 
-                if add_kaithia:
+            participants = [sender, recipient]
+
+            if add_kaithia:
                     participants.append("@kaithia_bot")
 
-                created_group = await event.client(functions.messages.CreateChatRequest(
+            created_group = await event.client(functions.messages.CreateChatRequest(
                     title=group_name,
                     users=participants,
                 ))
-
-                group_details = created_group.stringify()
-                chat_id_match = re.search(r'peerchat\s*\(\s*chat_id\s*=\s*(\d+)\s*\)', group_details, re.IGNORECASE)
                 
-                if chat_id_match:
+            group_details = created_group.stringify()
+                
+            chat_id_match = re.search(r'peerchat\s*\(\s*chat_id\s*=\s*(\d+)\s*\)', group_details, re.IGNORECASE)
+                
+            if chat_id_match:
                     chat_id = int(chat_id_match.group(1))
+                    
                     await event.client(functions.messages.EditChatAboutRequest(
                         peer=types.PeerChat(chat_id),
                         about="Created by @kaithia_bot"
@@ -68,11 +67,13 @@ async def check_command(event, client):
                     result = await client(functions.messages.ExportChatInviteRequest(peer=types.PeerChat(chat_id=chat_id), legacy_revoke_permanent=True))
                     invite_link = result.link
                     
+                    
                     await kaithia_message.edit(
-                        f"<b>@kaithia_bot</b> created group <b>{group_name}</b> successfully! \n\n Join the group using the link below: {invite_link}",
-                        parse_mode='HTML'
-                    )
-                else:
+                            f"<b>@kaithia_bot</b> created group <b>{group_name}</b> successfully! \n\n Join the group using the link below: {invite_link}",
+                            parse_mode='HTML'
+                        )
+
+            else:
                     await kaithia_message.edit(
                         f"<b>@Kaithia</b> created group <b>{group_name}</b> successfully!",
                         parse_mode='HTML'
@@ -80,106 +81,81 @@ async def check_command(event, client):
         except Exception as e:
             print(f"Error creating group: {str(e)}")
             if event.message:
-                await event.message.reply(f"Failed to create group: TL says {str(e)}")
+                await event.message.edit(f"Failed to create group: {str(e)}")
 
 async def load_client_for_session(session_file):
+    """Load a Telegram client for the given session file."""
     session_path = os.path.join(session_dir, session_file)
-    print(f"Attempting to load session from: {session_path}")
+    logging.info(f"Attempting to load session from: {session_path}") 
 
     client = TelegramClient(session_path, api_id, api_hash)
     
-    max_retries = 15
-    retries = 0
-    
-    while retries < max_retries:
-        try:
-            await client.connect()
+    try:
+        await client.connect()
 
-            if not await client.is_user_authorized():
-                print(f"Session for {session_file} is not authorized, login required.")
-                await client.disconnect()
-                
-                retries += 1
-                print(f"Retrying to load session for {session_file} in 20 seconds... (Attempt {retries}/{max_retries})")
-                await asyncio.sleep(20)
-                continue 
-            else:
-                print(f"Session for {session_file} authorized and connected!")
-
-            @client.on(events.NewMessage)
-            async def handle_message(event):
-                await check_command(event, client)
-
-            await client.start()
-            print(f"{session_file} connected successfully!")
-            clients.append(client)
-            return 
-
-        except Exception as e:
-            print(f"Failed to connect {session_file}: {str(e)}")
+        if not await client.is_user_authorized():
+            logging.warning(f"Session for {session_file} is not authorized, login required.")
             await client.disconnect()
-            return  
+            return
 
-async def main():
-    await load_existing_sessions()
-    await asyncio.gather(*[client.run_until_disconnected() for client in clients])
+        logging.info(f"Session for {session_file} authorized and connected!")
 
-async def load_existing_sessions():
+        @client.on(events.NewMessage)
+        async def handle_message(event):
+            await check_command(event, client)
+
+        await client.start()
+        logging.info(f"{session_file} connected successfully!")  
+        clients.append(client)
+
+    except Exception as e:
+        logging.error(f"Failed to connect {session_file}: {str(e)}")
+        await client.disconnect()
+
+
+async def load_clients():
+    """Load all existing client sessions."""
     session_files = [f for f in os.listdir(session_dir) if f.endswith('_session.session')]
-    
     for session_file in session_files:
         await load_client_for_session(session_file)
 
 
 
 class SessionFileEventHandler(FileSystemEventHandler):
+    """Handler for file system events to detect new session files."""
+    def __init__(self, loop):
+        self.loop = loop
+
     def on_created(self, event):
-        if event.is_directory:
+        if event.is_directory or not event.src_path.endswith('_session.session'):
             return
-        if event.src_path.endswith('_session.session'):
-            session_file = os.path.basename(event.src_path)
-            print(f"New session file detected: {session_file}")
-            asyncio.run(self.create_client_after_delay(session_file))
-
-    async def create_client_after_delay(self, session_file):
-        await asyncio.sleep(60)
-        await load_client_for_session(session_file)
+        session_file = os.path.basename(event.src_path)
+        logging.info(f"New session file detected: {session_file}")
+        asyncio.run_coroutine_threadsafe(load_client_for_session(session_file), self.loop)
 
 
-def start_observer():
-    event_handler = SessionFileEventHandler()
+
+def start_observer(loop):
+    """Start the file system observer to watch for new session files."""
+    event_handler = SessionFileEventHandler(loop)
     observer = Observer()
-    
     observer.schedule(event_handler, path=session_dir, recursive=False)
     observer.start()
     return observer
 
 
 
-
-
-async def shutdown_clients():
-    for client in clients:
-        await client.disconnect()
-    print("All clients disconnected.")
-
-
+async def main():
+    loop = asyncio.get_event_loop()
+    observer = start_observer(loop)
+    await load_clients()
+    try:
+        await asyncio.gather(*[client.run_until_disconnected() for client in clients])
+    finally:
+        observer.stop()
+        observer.join()
 
 
 
 if __name__ == "__main__":
-    observer = start_observer()
-
-    try:
-        while True:
-            try:
-                asyncio.run(main())
-            except Exception as e:
-                print(f"An error occurred: {str(e)}")
-                print("Restarting the bot in 5 seconds...")
-                time.sleep(5)
-    except KeyboardInterrupt:
-        observer.stop()
-    finally:
-        asyncio.run(shutdown_clients())
-    observer.join()
+    asyncio.run(main())
